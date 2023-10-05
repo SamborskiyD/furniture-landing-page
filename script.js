66f2752fbc477cdf4e004e783b48f9c85b4c63b5
dima_samborskyi <samborskiydmitriy1@gmail.com>
2023-10-05 17:58:27 +0300

    add js script

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function toggleTab(event, tabName) {
+    let tablinks = document.getElementsByClassName('products__navbar__item')
+    let tabs = document.getElementsByClassName('product')
+
+    for(let i = 0; i < tablinks.length; i++){
+        tablinks[i].classList.remove('products__navbar__item_active')
+    }
+
+    for(let i = 0; i < tabs.length; i++){
+        tabs[i].classList.remove('product_active')
+    }
+
+    event.target.classList.add('products__navbar__item_active')
+    document.getElementById(tabName).classList.add('product_active')
+}
